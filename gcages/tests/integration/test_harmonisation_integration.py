fd490d3cae659c74ae994116dc06ac8dce27332d
Zebedee Nicholls <zebedee.nicholls@climate-resource.com>
2025-01-09 17:16:33 +0100

    Add basic set up

---
+++ gcages/tests/integration/test_harmonisation_integration.py
@@ -0,0 +1,84 @@
+"""
+Integration tests of harmonisation
+"""
+
+from __future__ import annotations
+
+from pathlib import Path
+
+import pandas as pd
+import pandas_indexing as pix
+import pytest
+
+from gcages.ar6 import get_ar6_harmoniser
+
+TEST_DATA_DIR = Path(__file__).parents[1] / "test-data"
+
+
+@pytest.fixture(scope="session")
+def ar6_raw():
+    res = pd.read_csv(TEST_DATA_DIR / "ar6_IPs_emissions.csv")
+    res.columns = res.columns.str.lower()
+    res = res.set_index(["model", "scenario", "variable", "region", "unit"])
+    res.columns = res.columns.astype(int)
+
+    return res
+
+
+@pytest.fixture(scope="session")
+def ar6_harmonised_infilled():
+    res = pd.read_csv(TEST_DATA_DIR / "20220314_ar6emissions_harmonized_infilled.csv")
+    res.columns = res.columns.str.lower()
+    res = res.set_index(["model", "scenario", "variable", "region", "unit"])
+    res.columns = res.columns.astype(int)
+
+    return res
+
+
+@pytest.fixture(scope="session")
+def ar6_harmonised(ar6_harmonised_infilled):
+    res = ar6_harmonised_infilled.loc[pix.ismatch(variable="**Harmonized**")]
+
+    return res
+
+
+@pytest.fixture(scope="session")
+def ar6_infilled():
+    raise NotImplementedError()
+
+
+def create_harmonisation_test_cases():
+    model_scenarios = pd.read_csv(
+        TEST_DATA_DIR / "20220314_ar6emissions_harmonized_infilled_model_scenarios.csv"
+    )
+
+    return tuple(
+        pytest.param(model, scenario, id=f"{model}__{scenario}")
+        for (model, scenario), _ in model_scenarios.groupby(["Model", "Scenario"])
+    )
+
+
+harmonisation_cases = pytest.mark.parametrize(
+    "model, scenario", create_harmonisation_test_cases()
+)
+
+
+@harmonisation_cases
+def test_harmonisation(
+    model,
+    scenario,
+    ar6_raw,
+    ar6_harmonised,
+):
+    raw = ar6_raw.loc[pix.isin(model=model) & pix.isin(scenario=scenario)]
+    if raw.empty:
+        pytest.skip(f"No raw data for {model=} and {scenario=}")
+        return
+
+    harmoniser = get_ar6_harmoniser()
+
+    res = harmoniser.harmonise(raw)
+
+    exp = ar6_harmonised.loc[pix.isin(model=model) & pix.isin(scenario=scenario)]
+
+    pd.testing.assert_frame_equal(res, exp)
