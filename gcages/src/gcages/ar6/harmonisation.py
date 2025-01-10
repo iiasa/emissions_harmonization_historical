2d8e6f5d8f3f4e56340aa8a05d781827ccbe6901
Zebedee Nicholls <zebedee.nicholls@climate-resource.com>
2025-01-10 20:45:40 +0100

    Split out aneris helpers module

--- gcages/src/gcages/ar6/harmonisation.py
+++ gcages/src/gcages/ar6/harmonisation.py
@@ -6,12 +6,13 @@ from __future__ import annotations
 
 import importlib
 import multiprocessing
+from pathlib import Path
 
 import pandas as pd
 import pandas_indexing as pix  # type: ignore
-from aneris.harmonize import Harmonizer
 from attrs import define
 
+from gcages.aneris_helpers import harmonise_all
 from gcages.parallelisation import (
     assert_only_working_on_variable_unit_variations,
     run_parallel,
@@ -31,9 +32,8 @@ def load_ar6_historical_emissions() -> pd.DataFrame:
     :
         Historical emissions used in AR6
     """
-    res: pd.DataFrame = pd.read_csv(
-        importlib.resources.files("gcages") / "history_ar6.csv"
-    )
+    filepath: Path = Path(importlib.resources.files("gcages") / "history_ar6.csv")  # type: ignore
+    res: pd.DataFrame = pd.read_csv(filepath)
     res.columns = res.columns.str.lower()
     res = res.set_index(["model", "scenario", "variable", "unit", "region"])
     res.columns = res.columns.astype(int)
@@ -99,7 +99,6 @@ def add_historical_year_based_on_scaling(
         raise NotImplementedError
 
     ms = ("model", "scenario")
-    # emissions_no_ms = emissions.reset_index(ms, drop=True)
     emissions_historical_common_vars = emissions_historical.loc[
         pix.isin(variable=emissions.pix.unique("variable"))  # type: ignore
     ]
@@ -117,208 +116,6 @@ def add_historical_year_based_on_scaling(
     out = pd.concat([emissions, fill_value], axis="columns").sort_index(axis="columns")
 
     return out
-
-
-def _check_data(hist, scen, year):
-    # TODO: push back upstream
-    check = ["region", "variable"]
-
-    def downselect(df):
-        return pix.projectlevel(df.index, check)
-
-    s = downselect(scen)
-    h = downselect(hist)
-    if h.empty:
-        raise ValueError("No historical data in harmonization year")
-        # raise MissingHarmonisationYear("No historical data in harmonization year")
-
-    if not s.difference(h).empty:
-        # raise MissingHistoricalError(
-        raise ValueError(
-            "Historical data does not match scenario data in harmonization "
-            f"year for\n {s.difference(h)}"
-        )
-
-
-def _convert_units_to_match(
-    start: pd.DataFrame, match: pd.DataFrame, copy_on_entry: bool = True
-):
-    # TODO: push back upstream
-    if copy_on_entry:
-        out = start.copy()
-
-    else:
-        out = start
-
-    differences = pix.projectlevel(match.index, ["variable", "unit"]).difference(
-        pix.projectlevel(start.index, ["variable", "unit"])
-    )
-    if not differences.empty:
-        updated = []
-        for variable, target_unit in differences:
-            v_loc = pix.isin(variable=variable)
-            updated.append(out.loc[v_loc].pix.convert_unit(target_unit))
-            out = out.loc[~v_loc]
-
-        out = pix.concat([out, *updated])
-
-    return out
-
-
-def _knead_overrides(
-    overrides: pd.DataFrame, scen: pd.DataFrame, harm_idx: pd.MultiIndex
-) -> pd.DataFrame:
-    # TODO: push back upstream
-    """
-    Process overrides to get a form readable by aneris, supporting many different use cases.
-
-    Parameters
-    ----------
-    overrides : pd.DataFrame or pd.Series
-    scen : pyam.IamDataFrame with data for single scenario and model instance
-    """
-    if overrides is None:
-        return None
-
-    # massage into a known format
-    # check if no index and single value - this should be the override for everything
-    if overrides.index.names == [None] and len(overrides["method"]) == 1:
-        _overrides = pd.Series(
-            overrides["method"].iloc[0],
-            index=pd.Index(scen.region, name=harm_idx[-1]),  # only need to match 1 dim
-            name="method",
-        )
-    # if data is provided per model and scenario, get those explicitly
-    elif set(["model", "scenario"]).issubset(set(overrides.index.names)):
-        _overrides = overrides.loc[
-            pix.isin(model=scen.model, scenario=scen.scenario)
-        ].droplevel(["model", "scenario"])
-    # some of expected idx in cols, make it a multiindex
-    elif isinstance(overrides, pd.DataFrame) and set(harm_idx) & set(overrides.columns):
-        idx = list(set(harm_idx) & set(overrides.columns))
-        _overrides = overrides.set_index(idx)["method"]
-    else:
-        _overrides = overrides
-
-    # do checks
-    if isinstance(_overrides, pd.DataFrame) and _overrides.isnull().any(axis=None):
-        missing = _overrides.loc[_overrides.isnull().any(axis=1)]
-        # raise AmbiguousHarmonisationMethod(
-        raise ValueError(f"Overrides are missing for provided data:\n" f"{missing}")
-    if _overrides.index.to_frame().isnull().any(axis=None):
-        missing = _overrides[_overrides.index.to_frame().isnull().any(axis=1)]
-        # raise AmbiguousHarmonisationMethod(
-        raise ValueError(f"Defined overrides are missing data:\n" f"{missing}")
-    if _overrides.index.duplicated().any():
-        # raise AmbiguousHarmonisationMethod(
-        raise ValueError(
-            "Duplicated values for overrides:\n"
-            f"{_overrides[_overrides.index.duplicated()]}"
-        )
-
-    return _overrides
-
-
-def harmonise_all(
-    scenarios: pd.DataFrame,
-    history: pd.DataFrame,
-    year: int,
-    overrides: pd.DataFrame | None = None,
-):
-    """
-    Harmonise all timeseries in `scenarios` to match `history`
-
-    This is a re-write of aneris` version of the same.
-    TODO: MR upstream.
-
-    Parameters
-    ----------
-    scenarios
-        `pd.DataFrame` containing the timeseries to be harmonised
-
-    history
-        `pd.DataFrame` containing the historical timeseries to which
-        `scenarios` should be harmonised.
-
-    year
-        The year in which `scenarios` should be harmonised to `history`
-
-    overrides
-        If not provided, the default aneris decision tree is used.
-
-        Otherwise, `overrides` must be a `pd.DataFrame`
-        containing any specifications for overriding the default aneris methods.
-        Each row specifies one override.
-        The override method is specified in the "method" columns.
-        The other columns specify which of the timeseries in
-        `scenarios` should use this override by specifying metadata to match
-        ( e.g. variable, region).
-        If a cell has a null value (evaluated using `pd.isnull()`)
-        then that scenario characteristic will not be used for
-        filtering for that override.
-        For example, if you have a row with "method" equal to "constant_ratio",
-        region equal to "World" and variable is null
-        then all timeseries in the "World" region will use the "constant_ratio" method.
-        In contrast, if you have a row with "method" equal to "constant_ratio",
-        region equal to "World" and variable is "Emissions|CO2"
-        then only timeseries with variable equal to "Emissions|CO2"
-        and region equal to "World" will use the "constant_ratio" method.
-
-    Returns
-    -------
-    :
-        The harmonised timeseries
-
-    Notes
-    -----
-    This interface is nowhere near as sophisticated as aneris' other interfaces.
-    It simply harmonises timeseries.
-    It does not check sectoral sums or other possible errors which can arise when harmonising.
-    If you need such features, do not use this interface.
-
-    Raises
-    ------
-    MissingHistoricalError
-        No historical data is provided for a given timeseries
-
-    MissingHarmonisationYear
-        A value for the harmonisation year is missing or is null in `history`
-
-    AmbiguousHarmonisationMethod
-        `overrides` do not uniquely specify the harmonisation method for a given timeseries.
-    """
-    sidx = scenarios.index  # save in case we need to re-add extraneous indicies later
-
-    dfs = []
-    for (model, scenario), msdf in scenarios.groupby(["model", "scenario"]):
-        hist_msdf = history.loc[
-            pix.isin(region=msdf.pix.unique("region"))
-            & pix.isin(variable=msdf.pix.unique("variable"))
-        ]
-        _check_data(hist_msdf, msdf, year)
-        # pix.set_openscm_registry_as_default()
-        # hist_msdf = hist_msdf.pix.convert_unit({"Mt BC/yr": "Mt BC / yr"})
-        hist_msdf = _convert_units_to_match(start=hist_msdf, match=msdf)
-        # need to convert to internal datastructure
-        level_order = ["model", "scenario", "region", "variable", "unit"]
-        msdf_aneris = msdf.reorder_levels(level_order)
-        hist_msdf_aneris = hist_msdf.reorder_levels(level_order)
-        h = Harmonizer(msdf_aneris, hist_msdf_aneris, harm_idx=["variable", "region"])
-        # knead overrides
-        _overrides = _knead_overrides(overrides, msdf, harm_idx=["variable", "region"])
-        result = h.harmonize(year=year, overrides=_overrides)
-        # need to convert out of internal datastructure
-        dfs.append(
-            result.assign(model=model, scenario=scenario).set_index(
-                ["model", "scenario"], append=True
-            )
-        )
-
-    # realign indicies if more than standard IAMC_IDX were there originally
-    result = pix.concat(dfs)
-    result = pix.semijoin(result, sidx, how="right").reorder_levels(sidx.names)
-
-    return result
 
 
 def harmonise_scenario(
@@ -386,13 +183,14 @@ def harmonise_scenario(
         ~(emissions_to_harmonise[year] == 0.0)
     ]
 
-    # In AR6, we interpolated before harmonising
+    ### In AR6, we interpolated before harmonising
+
     # First, check that there are no nans in the max year.
     # I don't know what happens in that case.
     if emissions_to_harmonise[emissions_to_harmonise.columns.max()].isnull().any():
         raise NotImplementedError
 
-    # Then interpolate
+    # Then, interpolate
     out_interp_years = list(range(year, emissions_to_harmonise.columns.max() + 1))
     emissions_to_harmonise = emissions_to_harmonise.reindex(
         columns=out_interp_years
@@ -496,7 +294,7 @@ class AR6Harmoniser:
         # May need to drop out variables which are zero in the harmonisation year
 
         harmonised_df = pix.concat(
-            run_parallel(  # type: ignore
+            run_parallel(
                 func_to_call=harmonise_scenario,
                 iterable_input=(
                     gdf for _, gdf in in_emissions.groupby(["model", "scenario"])
