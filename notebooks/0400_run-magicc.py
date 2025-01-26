Fix bug in leftovers scaling

notebooks/0400_run-magicc.py
@@ -21,13 +21,14 @@
 # %%
 import json
 import os
-import random
 
+import numpy as np
 import openscm_runner
 import openscm_runner.adapters
 import openscm_runner.run
 import pandas as pd
 import pandas_indexing as pix
+import pint
 import pymagicc.definitions
 import scmdata
 
@@ -39,6 +40,9 @@ from emissions_harmonization_historical.constants import (
     INFILLING_WMO_ID,
 )
 from emissions_harmonization_historical.io import load_csv
+
+# %%
+pix.set_openscm_registry_as_default()
 
 # %%
 SCENARIO_TIME_ID = "20250122-140031"
@@ -59,7 +63,10 @@ N_CFGS_TO_RUN = 600
 # %%
 magicc_exe_path = DATA_ROOT.parents[0] / "magicc" / "magicc-v7.6.0a3" / "bin" / "magicc-darwin-arm64"
 magicc_expected_version = "v7.6.0a3"
-PROBABILISTIC_DISTRIBUTION_FILE = DATA_ROOT.parents[0] / "magicc" / "magicc-v7.6.0a3" / "configs" / "magicc-ar7-fast-track-drawnset-v0-3-0.json"
+PROBABILISTIC_DISTRIBUTION_FILE = (
+    DATA_ROOT.parents[0] / "magicc" / "magicc-v7.6.0a3" / "configs" / "magicc-ar7-fast-track-drawnset-v0-3-0.json"
+)
+
 # magicc_exe_path = DATA_ROOT.parents[0] / "magicc" / "magicc-v7.5.3" / "bin" / "magicc-darwin-arm64"
 # # Needed for 7.5.3 on a mac
 # os.environ["DYLD_LIBRARY_PATH"] = "/opt/homebrew/opt/gfortran/lib/gcc/current/"
@@ -75,11 +82,13 @@ magicc7 = openscm_runner.adapters.MAGICC7
 # %%
 if magicc7.get_version() != magicc_expected_version:
     raise AssertionError(magicc7.get_version())
-    
+
 magicc_expected_version
 
 # %%
 scenarios_raw = load_csv(complete_scenarios_file)
+# TODO: remove once we have data post 2100
+scenarios_raw = scenarios_raw.loc[:, :2100]
 scenarios_raw
 
 # %%
@@ -103,7 +112,7 @@ scenarios_raw
 #             n_selected += 1
 #             if n_selected >= 2:
 #                 break
-    
+
 #     else:
 #         if n_selected >= 1:
 #             selected_scenarios_l.append(sdf.loc[option_loc, :])
@@ -116,30 +125,32 @@ scenarios_raw
 #             selected_model = sdf.loc[option_loc, :].model
 #             selected_scenarios_l.append(sdf.loc[option_loc, :])
 #             selected_models.append(selected_model)
-        
+
 # selected_scenarios = pd.concat(selected_scenarios_l, axis="columns").T
 # selected_scenarios_idx = selected_scenarios.set_index(["model", "scenario"]).index
 # selected_scenarios
 
 # %%
-selected_scenarios_idx = pd.MultiIndex.from_tuples((
-    ('MESSAGEix-GLOBIOM 2.1-M-R12', 'SSP5 - High Emissions'),
-    ('IMAGE 3.4', 'SSP5 - High Emissions'),
-    ('AIM 3.0', 'SSP2 - Medium-Low Emissions'),
-    ('WITCH 6.0', 'SSP2 - Low Emissions'),
-    ('REMIND-MAgPIE 3.4-4.8', 'SSP2 - Low Overshoot_b'),
-    ('MESSAGEix-GLOBIOM-GAINS 2.1-M-R12', 'SSP5 - Low Overshoot'),
-    ('COFFEE 1.5', 'SSP2 - Medium Emissions'),
-    ('GCAM 7.1 scenarioMIP', 'SSP2 - Medium Emissions'),
-    ('IMAGE 3.4', 'SSP2 - Very Low Emissions'),
-    ('MESSAGEix-GLOBIOM-GAINS 2.1-M-R12', 'SSP1 - Very Low Emissions')
-    ), 
+selected_scenarios_idx = pd.MultiIndex.from_tuples(
+    (
+        # ('MESSAGEix-GLOBIOM 2.1-M-R12', 'SSP5 - High Emissions'),
+        # ('IMAGE 3.4', 'SSP5 - High Emissions'),
+        # ('AIM 3.0', 'SSP2 - Medium-Low Emissions'),
+        # ('WITCH 6.0', 'SSP2 - Low Emissions'),
+        # ('REMIND-MAgPIE 3.4-4.8', 'SSP2 - Low Overshoot_b'),
+        # ('MESSAGEix-GLOBIOM-GAINS 2.1-M-R12', 'SSP5 - Low Overshoot'),
+        # ('COFFEE 1.5', 'SSP2 - Medium Emissions'),
+        # ('GCAM 7.1 scenarioMIP', 'SSP2 - Medium Emissions'),
+        ("IMAGE 3.4", "SSP2 - Very Low Emissions"),
+        ("MESSAGEix-GLOBIOM-GAINS 2.1-M-R12", "SSP1 - Very Low Emissions"),
+    ),
     name=["model", "scenario"],
 )
 selected_scenarios_idx
 
 # %%
-scenarios_run = scenarios_raw[scenarios_raw.index.isin(selected_scenarios_idx)]
+# scenarios_run = scenarios_raw[scenarios_raw.index.isin(selected_scenarios_idx)]
+scenarios_run = scenarios_raw.loc[pix.ismatch(scenario="*Very Low*")]
 scenarios_run
 
 
@@ -152,9 +163,18 @@ def transform_iacm_to_openscm_runner_variable(v):
         ("CFC|", ""),
         ("HFC|", ""),
         ("|Montreal Gases", ""),
-        ("HFC43-10", "HFC4310mee", ),
-        ("AFOLU", "MAGICC AFOLU", ),
-        ("Energy and Industrial Processes", "MAGICC Fossil and Industrial", ),
+        (
+            "HFC43-10",
+            "HFC4310mee",
+        ),
+        (
+            "AFOLU",
+            "MAGICC AFOLU",
+        ),
+        (
+            "Energy and Industrial Processes",
+            "MAGICC Fossil and Industrial",
+        ),
     )
     for old, new in replacements:
         res = res.replace(old, new)
@@ -165,10 +185,72 @@ def transform_iacm_to_openscm_runner_variable(v):
 # %%
 scenarios_run_openscmrunner = scenarios_run.copy()
 scenarios_run_openscmrunner = scenarios_run_openscmrunner.pix.assign(
-    variable=scenarios_run_openscmrunner.index.get_level_values("variable").map(transform_iacm_to_openscm_runner_variable).values
+    variable=scenarios_run_openscmrunner.index.get_level_values("variable")
+    .map(transform_iacm_to_openscm_runner_variable)
+    .values
 )
 # Have to interpolate too before passing to OpenSCM-Runner, particularly MAGICC
 scenarios_run_openscmrunner = scenarios_run_openscmrunner.T.interpolate("index").T
+scenarios_run_openscmrunner
+
+
+# %%
+def transform_rcmip_to_iamc_variable(v):
+    """Transform RCMIP variables to IAMC variables"""
+    res = v
+
+    replacements = (
+        ("F-Gases|", ""),
+        ("PFC|", ""),
+        ("HFC4310mee", "HFC43-10"),
+        ("MAGICC AFOLU", "AFOLU"),
+        ("MAGICC Fossil and Industrial", "Energy and Industrial Processes"),
+    )
+    for old, new in replacements:
+        res = res.replace(old, new)
+
+    return res
+
+
+# %%
+MAGICC_FORCE_START_YEAR = 2015
+endyear = 2105  # add the 'MAGICC' buffer
+
+RCMIP_PATH = DATA_ROOT / "global/rcmip/data_raw/rcmip-emissions-annual-means-v5-1-0.csv"
+
+rcmip = pd.read_csv(RCMIP_PATH)
+rcmip_clean = rcmip.copy()
+rcmip_clean.columns = rcmip_clean.columns.str.lower()
+rcmip_clean = rcmip_clean.set_index(["model", "scenario", "region", "variable", "unit", "mip_era", "activity_id"])
+rcmip_clean.columns = rcmip_clean.columns.astype(int)
+rcmip_clean = rcmip_clean.pix.assign(
+    variable=rcmip_clean.index.get_level_values("variable")
+    .map(transform_rcmip_to_iamc_variable)
+    .map(transform_iacm_to_openscm_runner_variable)
+)
+ar6_harmonisation_points = rcmip_clean.loc[
+    pix.ismatch(mip_era="CMIP6")
+    & pix.ismatch(scenario="ssp245")
+    & pix.ismatch(region="World")
+    & pix.ismatch(variable=scenarios_run_openscmrunner.pix.unique("variable"))
+].reset_index(["mip_era", "activity_id"], drop=True)[MAGICC_FORCE_START_YEAR]
+with pint.get_application_registry().context("NOx_conversions"):
+    ar6_harmonisation_points = ar6_harmonisation_points.pix.convert_unit(
+        {"Mt NOx/yr": "Mt NO2/yr", "kt HFC4310mee/yr": "kt HFC4310/yr"}
+    )
+
+ar6_harmonisation_points
+
+# %%
+# Also have to add AR6 historical values in 2015 and interpolate,
+# because we haven't recalibrated MAGICC.
+a, b = ar6_harmonisation_points.reset_index(["model", "scenario"], drop=True).align(scenarios_run_openscmrunner)
+scenarios_run_openscmrunner = pix.concat([a.to_frame(), b], axis="columns").sort_index(axis="columns")
+for y in range(MAGICC_FORCE_START_YEAR, endyear + 1):
+    if y not in scenarios_run_openscmrunner:
+        scenarios_run_openscmrunner[y] = np.nan
+
+scenarios_run_openscmrunner = scenarios_run_openscmrunner.sort_index(axis="columns").T.interpolate("index").T
 scenarios_run_openscmrunner
 
 # %%
@@ -185,51 +267,50 @@ base_cfgs = [
 
 # %%
 startyear = 1750
-endyear = 2105  # add the 'MAGICC' buffer
 common_cfg = {
     "startyear": startyear,
     "endyear": endyear,
     "out_dynamic_vars": [
         "DAT_SURFACE_TEMP",
-        'DAT_SURFACE_MIXEDLAYERTEMP',
-        'DAT_TOTAL_INCLVOLCANIC_ERF',
-        'DAT_TOTAL_ANTHRO_ERF',
-        'DAT_AEROSOL_ERF',
-        'DAT_TOTAER_DIR_ERF',
-        'DAT_BCT_ERF',
-        'DAT_OCT_ERF',
-        'DAT_SOXT_ERF',
-        'DAT_CLOUD_TOT_ERF',
-        'DAT_GHG_ERF',
-        'DAT_CO2_ERF',
-        'DAT_CH4_ERF',
-        'DAT_N2O_ERF',
-        'DAT_FGASSUM_ERF',
-        'DAT_MHALOSUM_ERF',
-        'DAT_CFC11_ERF',
-        'DAT_CFC12_ERF',
-        'DAT_HCFC22_ERF',
-        'DAT_OZTOTAL_ERF',
-        'DAT_HFC125_ERF',
-        'DAT_HFC134A_ERF',
-        'DAT_HFC143A_ERF',
-        'DAT_HFC227EA_ERF',
-        'DAT_HFC23_ERF',
-        'DAT_HFC245FA_ERF',
-        'DAT_HFC32_ERF',
-        'DAT_HFC4310_ERFmee',
-        'DAT_CF4_ERF',
-        'DAT_C6F14_ERF',
-        'DAT_C2F6_ERF',
-        'DAT_SF6_ERF',
-        'DAT_HEATUPTK_EARTH',
-        'DAT_CO2_CONC',
-        'DAT_CH4_CONC',
-        'DAT_N2O_CONC',
-        'DAT_CO2_AIR2LAND_FLUX',
-        'DAT_CO2_AIR2OCEAN_FLUX',
-        'DAT_CO2PF_EMIS',
-        'DAT_CH4PF_EMIS'
+        "DAT_SURFACE_MIXEDLAYERTEMP",
+        "DAT_TOTAL_INCLVOLCANIC_ERF",
+        "DAT_TOTAL_ANTHRO_ERF",
+        "DAT_AEROSOL_ERF",
+        "DAT_TOTAER_DIR_ERF",
+        "DAT_BCT_ERF",
+        "DAT_OCT_ERF",
+        "DAT_SOXT_ERF",
+        "DAT_CLOUD_TOT_ERF",
+        "DAT_GHG_ERF",
+        "DAT_CO2_ERF",
+        "DAT_CH4_ERF",
+        "DAT_N2O_ERF",
+        "DAT_FGASSUM_ERF",
+        "DAT_MHALOSUM_ERF",
+        "DAT_CFC11_ERF",
+        "DAT_CFC12_ERF",
+        "DAT_HCFC22_ERF",
+        "DAT_OZTOTAL_ERF",
+        "DAT_HFC125_ERF",
+        "DAT_HFC134A_ERF",
+        "DAT_HFC143A_ERF",
+        "DAT_HFC227EA_ERF",
+        "DAT_HFC23_ERF",
+        "DAT_HFC245FA_ERF",
+        "DAT_HFC32_ERF",
+        "DAT_HFC4310_ERFmee",
+        "DAT_CF4_ERF",
+        "DAT_C6F14_ERF",
+        "DAT_C2F6_ERF",
+        "DAT_SF6_ERF",
+        "DAT_HEATUPTK_EARTH",
+        "DAT_CO2_CONC",
+        "DAT_CH4_CONC",
+        "DAT_N2O_CONC",
+        "DAT_CO2_AIR2LAND_FLUX",
+        "DAT_CO2_AIR2OCEAN_FLUX",
+        "DAT_CO2PF_EMIS",
+        "DAT_CH4PF_EMIS",
     ],
     "out_ascii_binary": "BINARY",
     "out_binary_format": 2,
@@ -238,19 +319,17 @@ common_cfg = {
 
 # %%
 def get_openscm_runner_output_names(magicc_names):
+    """
+    Get output names for the call to OpenSCM-Runner
+    """
     return [
-        pymagicc.definitions.convert_magicc7_to_openscm_variables(
-            magiccvarname
-        ).replace("DAT_", "")
+        pymagicc.definitions.convert_magicc7_to_openscm_variables(magiccvarname).replace("DAT_", "")
         for magiccvarname in magicc_names
     ]
 
 
-
 # %%
-openscm_runner_output_variables = get_openscm_runner_output_names(
-    common_cfg["out_dynamic_vars"]
-)
+openscm_runner_output_variables = get_openscm_runner_output_names(common_cfg["out_dynamic_vars"])
 openscm_runner_output_variables
 
 
@@ -281,7 +360,7 @@ magicc_res = magicc_res_full.timeseries(time_axis="year").loc[pix.isin(region=["
 magicc_res
 
 # %%
-sorted(magicc_res.pix.unique("variable"))
+# sorted(magicc_res.pix.unique("variable"))
 
 # %%
 temperature_raw = magicc_res.loc[pix.isin(variable="Surface Air Temperature Change")]
@@ -294,55 +373,148 @@ pi_years = range(1850, 1900 + 1)
 
 # %%
 temperature_rel_pi = temperature_raw.subtract(temperature_raw.loc[:, pi_years].mean(axis="columns"), axis="rows")
-temperature_shift = target_median - temperature_rel_pi.loc[:, target_median_years].mean(axis="columns").groupby(["model", "scenario"]).median()
+temperature_shift = (
+    target_median
+    - temperature_rel_pi.loc[:, target_median_years].mean(axis="columns").groupby(["model", "scenario"]).median()
+)
 temperature_match_historical_assessment = temperature_rel_pi.add(temperature_shift, axis="rows")
-temperature_match_historical_assessment.loc[:, target_median_years].mean(axis="columns").groupby(["model", "scenario"]).median()
+temperature_match_historical_assessment.loc[:, target_median_years].mean(axis="columns").groupby(
+    ["model", "scenario"]
+).median()
 
 # %%
-peak_warming_quantiles = temperature_match_historical_assessment.max(axis="columns").groupby(["model", "scenario"]).quantile([0.05, 0.17, 0.5, 0.83, 0.95]).unstack().sort_values(by=0.5)
+ax = (
+    temperature_match_historical_assessment.loc[:, 2000:2100]
+    .groupby(["model", "scenario", "region", "variable", "unit"])
+    .median()
+    .reset_index("region", drop=True)
+    .T.plot()
+)
+ax.legend(loc="center left", bbox_to_anchor=(1.05, 0.5))
+ax.grid()
+
+# %%
+cumulative_emms = (
+    scenarios_run_openscmrunner.loc[pix.ismatch(variable="**CO2**"), 2020:]
+    .groupby(scenarios_run.index.names.difference(["variable"]))
+    .sum(min_count=2)
+    .reset_index("region", drop=True)
+    .T.cumsum()
+    * 1.65
+    * 12
+    / 44
+    / 1e6
+)
+
+ax = cumulative_emms.plot()
+ax.legend(loc="center left", bbox_to_anchor=(1.05, 0.5))
+# ax.set_ylim([0, 1e6])
+
+# %%
+# Really want warming decomposition here
+methane_emms = scenarios_run_openscmrunner.loc[pix.ismatch(variable="**CH4"), 2020:].reset_index("region", drop=True).T
+
+ax = methane_emms.plot()
+ax.legend(loc="center left", bbox_to_anchor=(1.05, 0.5))
+ax.set_ylim(ymin=0)
+
+# %%
+# Really want warming decomposition here
+sulfur_emms = (
+    scenarios_run_openscmrunner.loc[pix.ismatch(variable="**Sulfur"), 2020:].reset_index("region", drop=True).T
+)
+
+ax = sulfur_emms.plot()
+ax.legend(loc="center left", bbox_to_anchor=(1.05, 0.5))
+ax.set_ylim(ymin=0)
+
+# %%
+peak_warming_quantiles = (
+    temperature_match_historical_assessment.max(axis="columns")
+    .groupby(["model", "scenario"])
+    .quantile([0.05, 0.17, 0.33, 0.5, 0.67, 0.83, 0.95])
+    .unstack()
+    .sort_values(by=0.33)
+)
 peak_warming_quantiles
 
 # %%
-eoc_warming_quantiles = temperature_match_historical_assessment[2100].groupby(["model", "scenario"]).quantile([0.05, 0.17, 0.5, 0.83, 0.95]).unstack().sort_values(by=0.5)
+eoc_warming_quantiles = (
+    temperature_match_historical_assessment[2100]
+    .groupby(["model", "scenario"])
+    .quantile([0.05, 0.17, 0.5, 0.83, 0.95])
+    .unstack()
+    .sort_values(by=0.5)
+)
 eoc_warming_quantiles
 
 # %%
 categories = pd.Series(
     "C8: Above 4.0°C",
-    index=peak_warming_quantiles.index.
+    index=peak_warming_quantiles.index,
     name="Category_name",
 )
 
-categories[peak_warming_quantiles[0.5] < 4.0] = "C7: Below 4.0°C"
-categories[peak_warming_quantiles[0.5] < 3.0] = "C6: Below 3.0°C"
-categories[peak_warming_quantiles[0.5] < 2.5] = "C5: Below 2.5°C"
-categories[peak_warming_quantiles[0.5] < 2.0] = "C4: Below 2.0°C"
-categories[peak_warming_quantiles[0.67] < 2.0] = "C3: Likely below 2°C"
-categories[(peak_warming_quantiles[0.33] > 1.5) & (eoc_warming_quantiles[0.5] < 1.5)] = "C2: Below 1.5°C with high OS"
-categories[(peak_warming_quantiles[0.33] <= 1.5) & (eoc_warming_quantiles[0.5] < 1.5)] = "C1b: Below 1.5°C with low OS"
-categories[peak_warming_quantiles[0.5] < 1.5] = "C1a: Below 1.5°C with no OS"
+categories[peak_warming_quantiles[0.5] < 4.0] = "C7: Below 4.0°C"  # noqa: PLR2004
+categories[peak_warming_quantiles[0.5] < 3.0] = "C6: Below 3.0°C"  # noqa: PLR2004
+categories[peak_warming_quantiles[0.5] < 2.5] = "C5: Below 2.5°C"  # noqa: PLR2004
+categories[peak_warming_quantiles[0.5] < 2.0] = "C4: Below 2.0°C"  # noqa: PLR2004
+categories[peak_warming_quantiles[0.67] < 2.0] = "C3: Likely below 2°C"  # noqa: PLR2004
+categories[(peak_warming_quantiles[0.33] > 1.5) & (eoc_warming_quantiles[0.5] < 1.5)] = "C2: Below 1.5°C with high OS"  # noqa: PLR2004
+categories[(peak_warming_quantiles[0.33] <= 1.5) & (eoc_warming_quantiles[0.5] < 1.5)] = "C1b: Below 1.5°C with low OS"  # noqa: PLR2004
+categories[peak_warming_quantiles[0.5] < 1.5] = "C1a: Below 1.5°C with no OS"  # noqa: PLR2004
 
 categories
 
 
 # %%
 def get_exceedance_probability(indf: pd.DataFrame, warming_level: float) -> float:
+    """
+    Get exceedance probability
+
+    For exceedance probability over time
+    (i.e. at each timestep, rather than at any point in the simulation),
+    see `get_exceedance_probability_over_time`
+    """
     peaks = indf.max(axis="columns")
-    ep = (peaks > warming_level) / peaks.shape[0] * 100
+    n_above_level = (peaks > warming_level).sum(axis="rows")
+    ep = n_above_level / peaks.shape[0] * 100
 
     return ep
 
 
 # %%
-exceedance_probabilities_l = [
-    temperature_match_historical_assessment.groupby(["model", "scenario"]).apply(get_exceedance_probability, warming_level=gwl)
-    for gwl in [1.5, 2.0]
-]
-pix.concat(exceedance_probabilities_l)
+exceedance_probabilities_l = []
+for gwl in [1.5, 2.0, 2.5]:
+    gwl_exceedance_probabilities_l = []
+    for (model, scenario), msdf in temperature_match_historical_assessment.groupby(["model", "scenario"]):
+        ep = get_exceedance_probability(msdf, warming_level=gwl)
+        ep_s = pd.Series(
+            ep,
+            name=f"{gwl:.2f}°C exceedance probability",
+            index=pd.MultiIndex.from_tuples(((model, scenario),), names=["model", "scenario"]),
+        )
+        gwl_exceedance_probabilities_l.append(ep_s)
+
+    exceedance_probabilities_l.append(pix.concat(gwl_exceedance_probabilities_l))
+
+exceedance_probabilities = (
+    pix.concat(exceedance_probabilities_l, axis="columns").melt(ignore_index=False).pix.assign(unit="%")
+)
+exceedance_probabilities = exceedance_probabilities.pivot_table(
+    values="value", columns="variable", index=exceedance_probabilities.index.names
+).sort_values("1.50°C exceedance probability")
+exceedance_probabilities
 
 
 # %%
 def get_exceedance_probability_over_time(indf: pd.DataFrame, warming_level: float) -> pd.Series:
+    """
+    Get exceedance probability over time
+
+    For exceedance probability at any point in the simulation,
+    see `get_exceedance_probability`
+    """
     gt_wl = (indf > warming_level).sum(axis="rows")
     ep = gt_wl / indf.shape[0] * 100
 
@@ -350,10 +522,26 @@ def get_exceedance_probability_over_time(indf: pd.DataFrame, warming_level: floa
 
 
 # %%
-exceedance_probabilities_over_time_l = [
-    temperature_match_historical_assessment.groupby(["model", "scenario"]).apply(get_exceedance_probability_over_time, warming_level=gwl)
-    for gwl in [1.5, 2.0]
-]
-pix.concat(exceedance_probabilities_over_time_l)
+exceedance_probabilities_l = []
+for gwl in [1.5, 2.0, 2.5]:
+    ep = (
+        temperature_match_historical_assessment.groupby(
+            temperature_match_historical_assessment.index.names.difference(["variable", "unit", "run_id"])
+        )
+        .apply(get_exceedance_probability_over_time, warming_level=gwl)
+        .pix.assign(unit="%", variable=f"{gwl:.2f}°C exceedance probability")
+    )
+
+    exceedance_probabilities_l.append(ep)
+
+exceedance_probabilities_over_time = pix.concat(exceedance_probabilities_l)
+exceedance_probabilities_over_time
 
 # %%
+ax = (
+    exceedance_probabilities_over_time.loc[pix.ismatch(variable="1.50*"), 2000:2100]
+    .reset_index(["region", "unit"], drop=True)
+    .T.plot()
+)
+ax.legend(loc="upper center", bbox_to_anchor=(0.5, -0.2))
+ax.grid()