fbae9d821adb97171bf76c6b2a3baed0af4f79a7
Zebedee Nicholls <zebedee.nicholls@climate-resource.com>
2025-01-31 20:13:28 +0100

    Create full history

--- notebooks/0108_wmo-2022-prepare.py
+++ notebooks/0108_wmo-2022-prepare.py
@@ -58,8 +58,8 @@ for gdir in raw_data_path.glob("*"):
     outputs_dir = gdir / "outputs"
     emissions_file = list(outputs_dir.glob("*Global_annual_emissions.csv"))
     if not emissions_file:
-        print(f"No data in {outputs_dir=}")
-        continue
+        msg = f"No data in {outputs_dir=}"
+        raise FileNotFoundError(msg)
 
     if len(emissions_file) != 1:
         raise NotImplementedError(list(outputs_dir.glob("*")))
