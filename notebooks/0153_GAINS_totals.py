acd0fdcf3d4312b10829a0a726c24c7378e9f633
jkikstra <kikstra@iiasa.ac.at>
2025-04-21 13:18:38 +0200

    Add GAINS processing (with guess for aggregation of duplicates)

--- notebooks/0153_GAINS_totals.py
+++ notebooks/0153_GAINS_totals.py
@@ -26,6 +26,8 @@ import pandas as pd
 import pandas_indexing as pix
 
 from emissions_harmonization_historical.constants import GAINS_PROCESSING_ID, DATA_ROOT
+from emissions_harmonization_historical.ceds import add_global
+from emissions_harmonization_historical.units import assert_units_match_wishes
 
 # %%
 pix.set_openscm_registry_as_default()
@@ -35,13 +37,12 @@ raw_data_path = DATA_ROOT / "national/gains/data_raw/"
 raw_data_path
 
 # %%
-adam_et_al_2022_processed_output_file = DATA_ROOT / Path(
-    "national",
-    "gains",
-    "processed",
-    f"gains-total_{GAINS_PROCESSING_ID}.csv",
+gains_processed_output_file_national = DATA_ROOT / Path(
+    "national", "gains", "processed", f"gains_national_{GAINS_PROCESSING_ID}.csv"
 )
-adam_et_al_2022_processed_output_file
+gains_processed_output_file_global = DATA_ROOT / Path(
+    "national", "gains", "processed", f"gains_global_{GAINS_PROCESSING_ID}.csv"
+)
 
 # %%
 in_file = raw_data_path / "country_hist_emissions.xlsx"
@@ -65,42 +66,146 @@ df = raw_df.rename(
         "EMISSION": "value",
     }, axis="columns"
 )
-df["region"] = df["region"].str.lower()
+df["country"] = df["region"].str.lower()
 df["model"] = "GAINS"
 df["scenario"] = "historical"
+
+# Drop PM2.5 here to avoid later unit registry issues
+df = df[df["variable"]!="PM_2_5"]
+
 # df = df.set_index(["model", "scenario", "region", "variable", "unit"])
-df = df.pivot(index=["model", "scenario", "region", "variable", "unit"], columns="year", values="value")
+df = df.pivot_table(index=["model", "scenario", "country", "variable", "unit"], columns="year", values="value", aggfunc="sum") # `aggfunc='sum'` is just a guess how to deal with the duplicates in the data 
+
+
+
 df
 
 # %%
 # change units to align with IAM scenario data
+gains = df
+
+# change name(s) of emissions species
+# use 'NOx' instead of 'NOX'
+gains.index = pd.MultiIndex.from_tuples(
+    [
+        (model, scenario, region, "NOx" if variable == "NOX" else variable, unit)
+        for model, scenario, region, variable, unit in gains.index
+    ],
+    names=gains.index.names,
+)
+# use 'BC' instead of 'PM_BC'
+gains.index = pd.MultiIndex.from_tuples(
+    [
+        (model, scenario, region, "BC" if variable == "PM_BC" else variable, unit)
+        for model, scenario, region, variable, unit in gains.index
+    ],
+    names=gains.index.names,
+)
+# use 'OC' instead of 'PM_OC'
+gains.index = pd.MultiIndex.from_tuples(
+    [
+        (model, scenario, region, "OC" if variable == "PM_OC" else variable, unit)
+        for model, scenario, region, variable, unit in gains.index
+    ],
+    names=gains.index.names,
+)
+
+# change unit(s) of emissions
 # adjust units; change column 'units' to 'unit' and add '/yr'
-df = df.pix.dropna(subset=["unit"]).pix.format(unit="{unit}/yr", drop=True)
-# # adjust units; change all to values to Mt instead of kt
-# fao = pix.units.convert_unit(fao, lambda x: "Mt " + x.removeprefix("kt").strip())
-# # exception for N2O/yr, which should remain in kt following https://github.com/IAMconsortium/common-definitions/
-# fao = pix.units.convert_unit(fao, lambda x: "kt " + x.removeprefix("Mt").strip() if x == "Mt N2O/yr" else x)
-# # unit of NOx from NOx to NO2
-# fao.index = pd.MultiIndex.from_tuples(
-#     [
-#         (country, fao_sectors, sector_description, em, sector, "Mt NO2/yr" if unit == "Mt NOx/yr" and em == "NOx" else unit)
-#         for country, fao_sectors, sector_description, em, sector, unit in fao.index
-#     ],
-#     names=fao.index.names,
-# )
-# # change name(s) of emissions species
-# # use 'Sulfur' instead of 'SO2'
-# fao.index = pd.MultiIndex.from_tuples(
-#     [
-#         (country, fao_sectors, sector_description, "Sulfur" if em == "SO2" else em, sector, unit)
-#         for country, fao_sectors, sector_description, em, sector, unit in fao.index
-#     ],
-#     names=fao.index.names,
-# )
-# fao.index.to_frame(index=False)
-df
+gains = gains.pix.dropna(subset=["unit"]).pix.format(unit="{unit} {variable}/yr", drop=False)
+# adjust units; change all to values to Mt instead of kt
+gains = pix.units.convert_unit(gains, lambda x: "Mt " + x.removeprefix("kt").strip())
+# exception for N2O/yr, which should remain in kt following https://github.com/IAMconsortium/common-definitions/
+gains = pix.units.convert_unit(gains, lambda x: "kt " + x.removeprefix("Mt").strip() if x == "Mt N2O/yr" else x)
+
+# change name(s) of emissions species that couldn't be changed before because Pint would have thrown an error in `convert_unit`
+# use 'Sulfur' instead of 'SO2'
+gains.index = pd.MultiIndex.from_tuples(
+    [
+        (model, scenario, region, "Sulfur" if variable == "SO2" else variable, unit)
+        for model, scenario, region, variable, unit in gains.index
+    ],
+    names=gains.index.names,
+)
+# also for units 
+# unit of NOx from NOx to NO2
+gains.index = pd.MultiIndex.from_tuples(
+    [
+        (model, scenario, region, variable, "Mt NO2/yr" if unit == "Mt NOx/yr" and variable == "NOx" else unit)
+        for model, scenario, region, variable, unit in gains.index
+    ],
+    names=gains.index.names,
+)
+
+# Show the data
+gains.index.to_frame(index=False)
 
 # %%
-adam_et_al_2022_processed_output_file.parent.mkdir(exist_ok=True, parents=True)
-out.to_csv(adam_et_al_2022_processed_output_file)
-adam_et_al_2022_processed_output_file
+gains = gains.groupby(["model", "scenario", "country", "variable", "unit"]).sum().pix.fixna()  # group and fix NAs
+
+# %%
+# aggregate countries where this is necessary, e.g. because of specific other data (like SSP socioeconomic driver data)
+# based on the new SSP data, we only need to aggregate Serbia and Kosovo
+country_combinations = {
+    # "isr_pse": ["isr", "pse"], "sdn_ssd": ["ssd", "sdn"],
+    "srb_ksv": ["srb", "srb (kosovo)"]
+}
+gains = gains.pix.aggregate(country=country_combinations)
+
+# %%
+# add global
+gains = add_global(gains, groups=["model", "scenario", "variable", "unit"])
+
+# %%
+gains_reformatted = gains.rename_axis(index={"country": "region"})
+gains_reformatted
+
+# %%
+# rename to IAMC-style variable names including standard index order
+gains_reformatted_iamc = (
+    gains_reformatted.pix.format(variable="Emissions|{variable}", drop=True)
+    .reorder_levels(["model", "scenario", "region", "variable", "unit"])
+).sort_values(by=["region", "variable"])
+gains_reformatted_iamc
+
+# %%
+gains_reformatted_iamc.pix.unique(['variable'])
+
+# %%
+assert_units_match_wishes(gains_reformatted_iamc)
+
+# %% [markdown]
+# Save formatted GAINS data
+
+# %%
+out_global = gains_reformatted_iamc.loc[pix.isin(region="World")]  # only the added "World" region
+out_national = gains_reformatted_iamc.loc[
+    ~pix.isin(region="World")
+] 
+
+# %% [markdown]
+# Check that national sums equal global total.
+
+# %%
+# Check that `out_national_with_global` totals (all countries in iso3c + CEDS 'global' region)
+# ... are the same as `out_global` totals ("World")
+national_sums_checker = (
+    pix.assignlevel(out_national.groupby(["model", "scenario", "variable", "unit"]).sum(), region="World")
+    .reset_index()
+    .set_index(out_global.index.names)
+)
+national_sums_checker.columns = national_sums_checker.columns.astype(int)
+national_sums_checker
+pd.testing.assert_frame_equal(out_global, national_sums_checker, check_like=True)
+
+# %%
+# national GAINS data
+gains_processed_output_file_national.parent.mkdir(exist_ok=True, parents=True)
+out_national.to_csv(gains_processed_output_file_national)
+gains_processed_output_file_national
+
+# %%
+# globally aggregated data (all emissions)
+gains_processed_output_file_global.parent.mkdir(exist_ok=True, parents=True)
+out_global.to_csv(gains_processed_output_file_global)
+gains_processed_output_file_global
