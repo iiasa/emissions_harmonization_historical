83e16a93c33e0231c51fffea8deff215d61e52a9
Marco Zecchetto <zecchetto@iiasa.ac.at>
2026-02-26 12:12:00 +0100

    moved extraction to script

---
+++ scripts/extract-ensemble-members.py
@@ -0,0 +1,61 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.18.1
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %%
+"""
+Extract the 600 ensemble members to csv
+"""
+
+# %%
+from pathlib import Path
+
+import pandas_indexing as pix
+import tqdm.auto
+
+from emissions_harmonization_historical.constants_5000 import (
+    DOWNLOAD_SCENARIOS_ID,
+    SCM_OUTPUT_DB,
+)
+
+
+# %%
+def main():
+    """
+    Extract the data
+    """
+    scm: str = "MAGICCv7.6.0a3"
+
+    HERE = Path(__file__).parent
+    REPO_ROOT = HERE.parent
+    OUT_PATH = REPO_ROOT / "ensemble-members" / DOWNLOAD_SCENARIOS_ID
+
+    raw_gsat_variable_in = "Surface Air Temperature Change"
+    iams = SCM_OUTPUT_DB.load_metadata().get_level_values("model").unique()
+
+    for model in tqdm.auto.tqdm(iams, desc="IAMs"):
+        gmt_df = SCM_OUTPUT_DB.load(
+            pix.ismatch(variable=raw_gsat_variable_in, model=f"*{model}*", climate_model=f"*{scm}*")
+        )
+
+        out_dir = OUT_PATH / model
+        out_dir.mkdir(exist_ok=True, parents=True)
+        out_file = out_dir / f"{model}_{scm}_ensemble_members.csv"
+
+        print(f"Extracting ensembles to {out_dir}")
+        gmt_df.to_csv(out_dir / out_file)
+
+
+# %%
+if __name__ == "__main__":
+    main()
